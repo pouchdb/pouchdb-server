cc2c873c070e853112f459b72105cf00a812df77
Nick Thompson <ncthom91@gmail.com>
2013-03-23 20:43:04 -0400

    Bug fix in bulkDocs route

--- index.js
+++ index.js
@@ -127,11 +127,9 @@ app.post('/:db/_bulk_docs', function (req, res, next) {
   // Maybe this should be moved into the leveldb adapter itself? Not sure
   // how uncommon it is for important options to come through in the body
   // https://github.com/daleharvey/pouchdb/issues/435
-  var opts = {
-    new_edits: 'new_edits' in req.body
-      ? req.body.new_edits
-      : undefined
-  };
+  var opts = 'new_edits' in req.body
+    ? { new_edits: req.body.new_edits }
+    : null;
 
   req.db.bulkDocs(req.body, opts, function (err, response) {
     if (err) return res.send(400, err);
