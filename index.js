3c27f88e2efcbb9ecf8b30b07207c58f2d83c055
Marten de Vries <marten94@gmail.com>
2014-05-08 15:49:40 +0200

    Fixed test regression on Ubuntu 12.04; switched to bluebird on node (and node only).

--- index.js
+++ index.js
@@ -18,7 +18,7 @@
 
 //TODO: call http equivalent if http adapter
 
-var Promise = require("lie");
+var Promise = require("bluebird");
 
 var coucheval = require("../utils/coucheval.js");
 var addCallback = require("../utils/promisewrapper.js");
