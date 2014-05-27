5dc7d3619eecfb46e8f73338cfb95c5ed95d673e
Marten de Vries <marten94@gmail.com>
2014-05-27 15:28:23 +0200

    Show and list now directly call _show and _list when using the HTTP adapter. Validation doesn't occur offline if the server handles it. No test suite regressions, but both are still experimental. (And JS coverage is lower.)

--- index.js
+++ index.js
@@ -1,32 +1,32 @@
 /*
-	Copyright 2013-2014, Marten de Vries
+  Copyright 2013-2014, Marten de Vries
 
-	Licensed under the Apache License, Version 2.0 (the "License");
-	you may not use this file except in compliance with the License.
-	You may obtain a copy of the License at
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
 
-	http://www.apache.org/licenses/LICENSE-2.0
+  http://www.apache.org/licenses/LICENSE-2.0
 
-	Unless required by applicable law or agreed to in writing, software
-	distributed under the License is distributed on an "AS IS" BASIS,
-	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-	See the License for the specific language governing permissions and
-	limitations under the License.
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
 */
 
 "use strict";
 
-//TODO: call http equivalent if http adapter.
-
 var extend = require("pouchdb-extend");
+var nodify = require("promise-nodify");
 
 var couchdb_objects = require("couchdb-objects");
 var render = require("couchdb-render");
-var nodify = require("promise-nodify");
+var httpQuery = require("couchdb-req-http-query");
 
 exports.list = function (listPath, options, callback) {
+  //options: values to end up in the request object of the list
+  //function (next to their defaults).
   var db = this;
-  var Promise = db.constructor.utils.Promise;
 
   if (typeof options === "function") {
     callback = options;
@@ -46,6 +46,20 @@ exports.list = function (listPath, options, callback) {
     return path;
   });
   var reqPromise = couchdb_objects.buildRequestObject(options, pathPromise, infoPromise, db);
+  return reqPromise.then(function (req) {
+    var promise;
+    if (["http", "https"].indexOf(db.type()) === -1) {
+      promise = offlineQuery(db, designDocName, listName, viewName, req, options);
+    } else {
+      promise = httpQuery(db, req);
+    }
+    nodify(promise, callback);
+    return promise;
+  });
+};
+
+function offlineQuery(db, designDocName, listName, viewName, req, options) {
+  var Promise = db.constructor.utils.Promise;
 
   //get the data involved.
   var ddocPromise = db.get("_design/" + designDocName).then(function (designDoc) {
@@ -60,10 +74,9 @@ exports.list = function (listPath, options, callback) {
   });
   var viewPromise = db.query(designDocName + "/" + viewName, options);
 
-  var promise = Promise.all([ddocPromise, viewPromise, reqPromise]).then(function (args) {
+  return Promise.all([ddocPromise, viewPromise]).then(function (args) {
     var designDoc = args[0];
     var viewResp = args[1];
-    var req = args[2];
 
     var head = {
       offset: viewResp.offset,
@@ -97,6 +110,4 @@ exports.list = function (listPath, options, callback) {
     }
     return resp;
   });
-  nodify(promise, callback);
-  return promise;
-};
+}
