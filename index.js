029595e5311751c1c5de45e86599ac1385d4c68b
Marten de Vries <marten94@gmail.com>
2014-05-28 20:29:23 +0200

    Update plug-in now included as public method. More consistent errors. A few bug fixes. Code coverage to 100% (python, js); no more fixmes/todos. Pretty much only pouchdb.mapping is now left as experimental.

---
+++ index.js
@@ -0,0 +1,70 @@
+/*
+  Copyright 2013-2014, Marten de Vries
+
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+  http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+*/
+
+"use strict";
+
+var extend = require("extend");
+var isEmpty = require("is-empty");
+
+module.exports = function completeRespObj(resp, contentType) {
+  //contentType may be undefined (if unknown). Resp may be anything
+  //returned by the user as response.
+
+  if (typeof resp === "string") {
+    resp = {body: resp};
+  }
+  if (Object.prototype.toString.call(resp) !== "[object Object]") {
+    resp = {body: ""};
+  }
+  //check for keys that shouldn't be in the resp object
+  var copy = extend({}, resp);
+  delete copy.code;
+  delete copy.json;
+  delete copy.body;
+  delete copy.base64;
+  delete copy.headers;
+  delete copy.stop;
+  if (!isEmpty(copy)) {
+    var key = Object.keys(copy)[0];
+    throw {
+      "status": 500,
+      "name": "external_response_error",
+      "message": (
+        "Invalid data from external server: {<<" +
+        JSON.stringify(key) +
+        ">>,<<" +
+        JSON.stringify(copy[key]) +
+        ">>}"
+      )
+    };
+  }
+  resp.code = resp.code || 200;
+  resp.headers = resp.headers || {};
+  resp.headers.Vary = resp.headers.Vary || "Accept";
+  //if a content type is known by now, use it.
+  resp.headers["Content-Type"] = resp.headers["Content-Type"] || contentType;
+  if (resp.json) {
+    resp.body = JSON.stringify(resp.json);
+    resp.headers["Content-Type"] = resp.headers["Content-Type"] || "application/json";
+  }
+  if (resp.base64) {
+    resp.headers["Content-Type"] = resp.headers["Content-Type"] || "application/binary";
+  }
+  //the default content type
+  resp.headers["Content-Type"] = resp.headers["Content-Type"] || "text/html; charset=utf-8";
+
+  return resp;
+};
