25050ee383a6c63338bf08d0baaf9de9ed014948
Nolan Lawson <nolan.lawson@gmail.com>
2014-06-20 18:58:54 -0700

    (pouchdb/pouchdb#2409) - remove circular dep

--- bin/test-pouchdb.sh
+++ bin/test-pouchdb.sh
@@ -3,8 +3,4 @@
 cd node_modules/pouchdb/
 npm install
 
-# TODO: dirty, figure out why npm link doesn't work
-rm -fr node_modules/pouchdb-server
-ln -s ../../.. node_modules/pouchdb-server
-
 SERVER=pouchdb-server npm test
