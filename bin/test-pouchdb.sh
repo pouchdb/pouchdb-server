6604e171b842c758ae9e77796c4c704793180103
Nolan Lawson <nolan.lawson@gmail.com>
2014-06-20 20:32:26 -0700

    (#49) - alt level backends

--- bin/test-pouchdb.sh
+++ bin/test-pouchdb.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
+./bin/pouchdb-server -p 6984 $SERVER_ARGS &
+POUCHDB_SERVER_PID=$!
+
 cd node_modules/pouchdb/
 npm install
 
-SERVER=pouchdb-server npm test
+COUCH_HOST=http://localhost:6984 npm test
+
+EXIT_STATUS=$?
+if [[ ! -z $POUCHDB_SERVER_PID ]]; then
+  kill $POUCHDB_SERVER_PID
+fi
+exit $EXIT_STATUS
