67e672654518d19c7e8c24becde1726d3c7170be
Douglas Christopher Wilson <doug@somethingdoug.com>
2014-07-27 13:26:53 -0400

    Option to disable logger

--- bin/test-couchdb.sh
+++ bin/test-couchdb.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-node bin/pouchdb-server -p 6984 &
+node bin/pouchdb-server -p 6984 -l off &
 export POUCHDB_SERVER_PID=$!
 ./node_modules/couchdb-harness/bin/couchdb-harness -p 6984
 
