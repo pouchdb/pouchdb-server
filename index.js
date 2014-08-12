2fb72eab578776b131970d6f60a0633314a7386e
Marten de Vries <marten94@gmail.com>
2014-08-12 21:59:58 +0200

    New: PouchDB Seamless Auth; PouchDB Security. Lots of fixes. PouchDB 3.0.

--- index.js
+++ index.js
@@ -17,56 +17,47 @@
 "use strict";
 
 var PouchPluginError = require("pouchdb-plugin-error");
+var extend = require("extend");
 
 exports.evaluate = function (requireContext, extraVars, program) {
-  /*jshint evil:true, unused: false */
+  /*jshint unused: false */
   var require;
   if (requireContext) {
     require = function (libPath) {
-      var module = {
-        id: libPath,
-        //no way to fill in current and parent that I know of
-        current: undefined,
-        parent: undefined,
-        exports: {}
-      };
-      var exports = module.exports;
+      var requireLocals = extend({
+        module: {
+          id: libPath,
+          //no way to fill in current and parent that I know of
+          current: undefined,
+          parent: undefined,
+          exports: {}
+        },
+      }, locals);
+      requireLocals.exports = requireLocals.module.exports;
 
       var path = libPath.split("/");
       var lib = requireContext;
       for (var i = 0; i < path.length; i += 1) {
         lib = lib[path[i]];
       }
-      eval(lib);
-      return module.exports;
+      lib += "\nreturn module.exports;";
+      return evalProgram(lib, requireLocals);
     };
-  }
-  var isArray = Array.isArray;
-  var toJSON = JSON.stringify;
-  var log = function (message) {
-    if (typeof message != "string") {
-      message = JSON.stringify(message);
-    }
-    console.log("EVALUATED FUNCTION LOGS: " + message);
-  };
-  var sum = function (array) {
-    return array.reduce(function (a, b) {
-      return a + b;
-    });
-  };
-
-  var statements = "";
-  for (var name in extraVars) {
-    if (extraVars.hasOwnProperty(name)) {
-      statements += "var " + name + " = extraVars['" + name + "'];\n";
-    }
   }
 
   //Strip trailing ';'s to make it more likely to be a valid expression
   program = program.replace(/;\s*$/, "");
+
+  var locals = extend({
+    isArray: isArray,
+    toJSON: toJSON,
+    log: log,
+    sum: sum,
+    require: require
+  }, extraVars);
   var func;
   try {
-    func = eval(statements + "(" + program + ");");
+    func = evalProgram("return " + program, locals);
     if (typeof func !== "function") {
       //activate the exception handling mechanism down here.
       throw "no function";
@@ -80,6 +71,35 @@ exports.evaluate = function (requireContext, extraVars, program) {
   }
   return func;
 };
+
+var isArray = Array.isArray;
+var toJSON = JSON.stringify;
+var log = function (message) {
+  if (typeof message != "string") {
+    message = JSON.stringify(message);
+  }
+  console.log("EVALUATED FUNCTION LOGS: " + message);
+};
+var sum = function (array) {
+  return array.reduce(function (a, b) {
+    return a + b;
+  });
+};
+
+function evalProgram(program, locals) {
+  /*jshint evil:true */
+  var keys = Object.keys(locals);
+  var values = keys.map(function (key) {
+    return locals[key];
+  });
+  var code = (
+    "(function (" + keys.join(", ") + ") {" +
+      program +
+    "})"
+  );
+
+  return eval(code).apply(null, values);
+}
 
 exports.wrapExecutionError = function (e) {
   return new PouchPluginError({
