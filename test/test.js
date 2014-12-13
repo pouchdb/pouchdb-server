9f94735416ec2647b20d96f56016257cae3fc6b6
Marten de Vries <marten94@gmail.com>
2014-12-13 21:55:24 +0100

    (pouchdb/pouchdb-server#72) - Reintroduce app.couchConfig

--- test/test.js
+++ test/test.js
@@ -11,7 +11,7 @@ var buildApp = require('..'),
 var TEST_DATA = __dirname + '/testdata/';
 var LARGE_TIMEOUT = 5000;
 
-var expressApp;
+var expressApp, expressApp2;
 
 before(function (done) {
   this.timeout(LARGE_TIMEOUT);
@@ -19,14 +19,24 @@ before(function (done) {
     if (err) {
       return done(err);
     }
-    fse.mkdir(TEST_DATA, function (err) {
+    fse.mkdirs(TEST_DATA + 'a', function (err) {
       if (err) {
         return done(err);
       }
-      expressApp = buildApp(PouchDB.defaults({
-        prefix: TEST_DATA
-      }));
-      done();
+      fse.mkdirs(TEST_DATA + 'b', function (err) {
+        if (err) {
+          return done(err);
+        }
+        expressApp = buildApp(PouchDB.defaults({
+          prefix: TEST_DATA + 'a/'
+        }));
+        expressApp2 = buildApp(PouchDB.defaults({
+          prefix: TEST_DATA + 'b/',
+        }), {
+          configPath: TEST_DATA + 'b-config.json'
+        });
+        done();
+      });
     });
   });
 });
@@ -53,5 +63,65 @@ prefixes.forEach(function (prefix) {
         })
         .end(done);
     });
+  });
+});
+
+describe('config', function () {
+  it('should have ./config.json as default config path', function (done) {
+    fse.exists('./config.json', function (exists) {
+      if (!exists) {
+        return done(new Error("config.json doesn't exist!"));
+      }
+      done();
+    });
+  });
+  it('should support setting a different config path', function (done) {
+    fse.exists(TEST_DATA + 'b-config.json', function (exists) {
+      if (!exists) {
+        return done(new Error("b-config.json doesn't exist!"));
+      }
+      done();
+    });
+  });
+  it('should support externally adding a default', function (done) {
+    expressApp.couchConfig.registerDefault('a', 'b', 'c');
+    request(expressApp)
+      .get('/_config')
+      .expect(200)
+      .expect(function (res) {
+        var a = JSON.parse(res.text).a;
+        if (!(typeof a === "object" && a.b === "c")) {
+          return "Default not shown";
+        }
+      })
+      .end(done);
+  });
+  it('should support externally getting a config value', function (done) {
+    request(expressApp)
+      .put('/_config/test/a')
+      .send('"b"')
+      .expect(200)
+      .end(function () {
+        if (expressApp.couchConfig.get('test', 'a') !== 'b') {
+          return done(new Error("Can't read setting that's just been set"));
+        }
+        done();
+      });
+  });
+  it('should support external listeners to a config change', function (done) {
+    var changed = false;
+    expressApp.couchConfig.on('test2.a', function () {
+      changed = true;
+    });
+    request(expressApp)
+      .put('/_config/test2/a')
+      .send('"b"')
+      .expect(200)
+      .expect(function (res) {
+        if (!changed) {
+          return "Didn't get notice of the setting change";
+        }
+      })
+      .end(done);
   });
 });
