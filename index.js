7038454d1b4c79ffc73e67bc1c4f4bdadfe27f05
Marten de Vries <marten94@gmail.com>
2014-12-19 14:21:15 +0100

    1.0.0

---
+++ index.js
@@ -0,0 +1,51 @@
+"use strict";
+
+var XHR = global.XMLHttpRequest;
+
+/* istanbul ignore else */
+if (typeof XHR === "undefined") {
+  XHR = require('xhr2');
+}
+var Promise = require('pouchdb-promise');
+var extend = require('extend');
+var wrappers = require('pouchdb-wrappers');
+
+module.exports = function (PouchDB, url, opts) {
+  var api = {};
+
+  api.allDbs = function () {
+    return new Promise(function (resolve, reject) {
+      var xhr = new XHR();
+      xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4) {
+          resolve(JSON.parse(xhr.responseText));
+        }
+      };
+      xhr.open('GET', url + '_all_dbs');
+      xhr.send();
+    });
+  };
+
+  api.destroy = function (orig, args) {
+    args.options.name = getName(args.options.name);
+
+    return orig();
+  };
+
+  function getName(name) {
+    if (!/https?:/.test(name)) {
+      name = url + name;
+    }
+    return name;
+  }
+
+  var getHost = new PouchDB('test', {adapter: 'http'}).getHost;
+  var HTTPPouchDB = PouchDB.defaults({
+    adapter: 'http',
+    getHost: function (name, specificOpts) {
+      return getHost(getName(name), extend({}, opts, specificOpts));
+    }
+  });
+  wrappers.installStaticWrapperMethods(HTTPPouchDB, api);
+  return HTTPPouchDB;
+};
---
+++ test/test.js
@@ -0,0 +1,85 @@
+"use strict";
+
+/* global describe, it */
+
+var PouchDB = require('pouchdb');
+//var buildHTTPPouchDB = require('http-pouchdb');
+var buildHTTPPouchDB = require('../index.js');
+var should = require('chai').should();
+
+var HTTPPouchDB = buildHTTPPouchDB(PouchDB, 'http://localhost:5984/');
+var XMLHttpRequest = require('xhr2');
+var Promise = require('pouchdb-promise');
+
+describe('constructor', function () {
+  it('should create remote databases for normal db names', function (done) {
+    var users = new HTTPPouchDB('_users');
+    users.info().then(function (info) {
+      // a couchdb-only property. Even pouchdb-size doesn't provide it.
+      info.should.have.property('data_size');
+    }).then(done);
+  });
+
+  it('should still accept http urls', function (done) {
+    var replicator = new HTTPPouchDB('http://localhost:5984/_replicator');
+    replicator.allDocs(function (err, resp) {
+      should.not.exist(err);
+      resp.should.have.property('rows');
+      // there's always the design doc
+      resp.total_rows.should.be.greaterThan(0);
+
+      done();
+    });
+  });
+});
+
+describe('destroy', function () {
+  it("should be possible using the 'class method'", function (done) {
+    new HTTPPouchDB('test');
+    PouchDB.destroy('test', done);
+  });
+  it('should be possible using the method', function (done) {
+    var db = new HTTPPouchDB('test');
+    db.destroy(done);
+  });
+});
+
+describe('replicate', function () {
+  it('should work', function (done) {
+    HTTPPouchDB.replicate('test-a', 'test-b').on('complete', function (resp) {
+      resp.status.should.equal('complete');
+
+      dbShouldExist('test-a').then(function () {
+        return dbShouldExist('test-b');
+      }).then(function () {
+        return HTTPPouchDB.destroy('http://localhost:5984/test-a');
+      }).then(function () {
+        HTTPPouchDB.destroy('test-b', done);
+      });
+    });
+  });
+});
+
+function dbShouldExist(name) {
+  return new Promise(function (resolve, reject) {
+    var xhr = new XMLHttpRequest();
+    xhr.onload = function () {
+      xhr.status.should.equal(200);
+      resolve();
+    };
+    xhr.open('HEAD', 'http://localhost:5984/' + name);
+    xhr.send();
+  });
+}
+
+describe('allDbs', function () {
+  it('should return the remote db list', function (done) {
+    HTTPPouchDB.allDbs(function (err, dbs) {
+      should.not.exist(err);
+      dbs.should.contain('_users');
+      dbs.should.contain('_replicator');
+
+      done();
+    });
+  });
+});
