ce5eb840f76bb1eb5e80d82ac3c6a6b24e67746e
Nolan Lawson <nolan.lawson@gmail.com>
2016-01-18 11:13:05 -0500

    (pouchdb/pouchdb#4798) - use git master for tests

--- bin/test-pouchdb.sh
+++ bin/test-pouchdb.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 
+# install pouchdb from git master rather than npm,
+# so we can run its own tests
+rm -fr node_modules/pouchdb
+git clone --depth 1 --single-branch --branch master \
+  https://github.com/pouchdb/pouchdb.git node_modules/pouchdb
+
+cd node_modules/pouchdb/
+npm install
+
+cd ../..
+
 ./bin/pouchdb-server -p 6984 $SERVER_ARGS &
 POUCHDB_SERVER_PID=$!
 
 cd node_modules/pouchdb/
-npm install
 
 COUCH_HOST=http://localhost:6984 npm test
 
