3fc555688ce8ab6bf2612c9dd71c3bd59eccf1ba
Garren Smith <garren.smith@gmail.com>
2017-02-17 13:21:18 +0200

    more

--- bin/test-setup.sh
+++ bin/test-setup.sh
@@ -5,6 +5,7 @@ echo "running"
 echo $DIRECTORY
 
 if [ ! -d "$DIRECTORY" ]; then
+  echo "cloning"
   # Control will enter here if $DIRECTORY exists.
   git clone --single-branch --branch master \
     https://github.com/pouchdb/pouchdb.git ${DIRECTORY}
