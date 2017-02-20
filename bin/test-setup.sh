66fc7890a4bf6864f5264d60543696fdb5fd8d64
Nolan Lawson <nolan@nolanlawson.com>
2017-02-19 19:28:03 -0800

    (#198) - improvements to test scripts

--- bin/test-setup.sh
+++ bin/test-setup.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 
+# Check out PouchDB itself from master and build it, so that we can
+# run its test suite.
+# Note: this will start to fail randomly if something changes in
+# PouchDB master. In those cases, just pin it to a specific Git commit.
+
 DIRECTORY='pouchdb-tests'
 
 if [ ! -d "$DIRECTORY" ]; then
   # Control will enter here if $DIRECTORY exists.
   git clone --single-branch --branch master \
+    --depth 1 \
     https://github.com/pouchdb/pouchdb.git ${DIRECTORY}
 fi
 
 cd pouchdb-tests
 npm install
-
 cd ..
