1543a570c2f3c5e6e4c03f27c0198e0306ed08b2
Garren Smith <garren.smith@gmail.com>
2017-02-21 14:20:39 +0200

    convert tests to es2015

--- tests/pouchdb-replicator/test.js
+++ tests/pouchdb-replicator/test.js
@@ -1,6 +1,8 @@
-import {PouchDB, setup, teardown, should, shouldThrowError} from 'pouchdb-plugin-helper/testutils';
-import Replicator from '../../packages/node_modules/pouchdb-replicator';
-import extend from 'extend';
+const Promise = require('bluebird');
+global.Promise = Promise; //use bluebird for all promises
+const {PouchDB, setup, teardown, should} = require('pouchdb-plugin-helper/testutils');
+const Replicator = require('../../packages/node_modules/pouchdb-replicator');
+const extend = require('extend');
 
 PouchDB.plugin(Replicator);
 
@@ -9,7 +11,7 @@ const replicationDocument = {
 	"source": "a",
 	"target": "b",
 	"continuous": true
-}
+};
 
 let db;
 
@@ -17,11 +19,13 @@ describe('async replicator tests', () => {
 	beforeEach(() => {
 		db = setup();
 	});
+
 	afterEach(teardown);
 
 	it('basic', done => {
 		db.startReplicator((err, res) => {
-			if (err) return done(err);
+			if (err) { return done(err); }
+
 
 			should.not.exist(res);
 			db.stopReplicator((err, res) => {
@@ -29,161 +33,255 @@ describe('async replicator tests', () => {
 				done(err);
 			});
 		});
-	})
+	});
 });
 
 describe('sync replicator tests', () => {
-	beforeEach(async () => {
+	beforeEach(() => {
 		db = setup();
 
-		should.not.exist(await db.startReplicator());
+		return db.startReplicator().then((err, res) => {
+			should.not.exist(res);
+		});
 	});
-	afterEach(async () => {
-		should.not.exist(await db.stopReplicator());
 
-		await new PouchDB('a').destroy();
-		await new PouchDB('b').destroy();
-
-		await teardown();
+	afterEach(() => {
+		return db.stopReplicator()
+		.then((err, res) => {
+			should.not.exist(res);
+			return new PouchDB('a').destroy();
+		}).then(() => {
+			return new PouchDB('b').destroy();
+		})
+		.then(teardown);
 	});
 
 	it('basic', () => {
 		// let beforeEach & afterEach do their job
 	});
 
-	it('start twice', async () => {
-		const err = await shouldThrowError(async () => {
-			await db.startReplicator();
+	it('start twice', () => {
+		return db.startReplicator()
+		.then(() => {
+			throw "should not get here";
+		}).catch(err => {
+			err.status.should.equal(500);
+			err.name.should.equal('already_active');
 		});
-		err.status.should.equal(500);
-		err.name.should.equal('already_active');
 	});
 
-	it('stop twice', async () => {
+	it('stop twice', () => {
 		// stop
-		await db.stopReplicator();
-
-		// second stop
-		const err = await shouldThrowError(async () => {
-			await db.stopReplicator();
+		return db.stopReplicator().then(() => {
+			return db.stopReplicator()
+				.then(() => {
+					throw "should not get here";
+				}).catch(err => {
+					err.toString().should.contain('500');
+					err.toString().should.contain('already_inactive');
+					return db.startReplicator();
+				});
 		});
-		err.toString().should.contain('500');
-		err.toString().should.contain('already_inactive');
-
-		// restart for afterEach
-		await db.startReplicator();
 	});
 
-	async function replicationCompletion(db, name) {
-		let doc;
-		do {
-			doc = await db.get(name);
-		} while (doc._replication_state !== 'completed');
-		return doc;
+	function replicationCompletion(db, name) {
+		return new Promise (resolve => {
+			const check = (doc) => {
+				if (doc._replication_state === 'completed') {
+					resolve(doc);
+					return true;
+				}
+
+				return async(resolve);
+			};
+			const async = () => db.get(name).then(check);
+
+			async();
+		});
 	}
 
-	it('replication validation', async () => {
-		const err = await shouldThrowError(async () => {
-			await db.post({});
+	it('replication validation', () => {
+		return db.post({}).catch(err => {
+			err.status.should.equal(403);
 		});
-		err.status.should.equal(403);
 	});
 
-	it('simple replication', async () => {
-		await new PouchDB('a').put({_id: 'test'});
-		const repDoc = extend({}, replicationDocument);
-		delete repDoc.continuous;
-		const resp = await db.put(repDoc);
-		resp.ok.should.be.ok;
-
-		await replicationCompletion(db, 'my_replication');
-
-		// fails if the document isn't there.
-		await new PouchDB('b').get('test');
+	it('simple replication', () => {
+		const dbSource = new PouchDB('a');
+		return dbSource.put({_id: 'test'})
+			.then(() => {
+				const repDoc = extend({}, replicationDocument);
+				delete repDoc.continuous;
+				return db.put(repDoc);
+			})
+			.then(resp => {
+				resp.ok.should.be.ok;
+				return replicationCompletion(db, 'my_replication');
+			})
+			.then(() => {
+				// fails if the document isn't there.
+				return new PouchDB('b').get('test');
+			})
+			.then (doc => {
+				return doc.should.exist;
+			})
+			.catch(() => {
+				throw "should not be here";
+			});
 	});
 
-	it('change replication', async () => {
-		await new PouchDB('a').put({}, 'test');
-		const repDoc = extend({}, replicationDocument);
-		delete repDoc.continuous;
-		const resp = await db.put(repDoc);
-		resp.ok.should.be.ok;
+	it('change replication', () => {
+		let dbC;
 
-		const doc = await replicationCompletion(db, 'my_replication');
+		return new PouchDB('a').put({_id: 'test'})
+		.then (() => {
+			const repDoc = extend({}, replicationDocument);
+			delete repDoc.continuous;
+			return db.put(repDoc);
+		})
+		.then(resp => {
+			resp.ok.should.be.ok;
+			return replicationCompletion(db, 'my_replication');
+		})
+		.then (doc => {
+			doc.source = 'b';
+			doc.target = 'c';
+			delete doc._replication_state;
+			return db.put(doc);
+		})
+		.then (resp2 => {
+			resp2.ok.should.be.ok;
+			return replicationCompletion(db, 'my_replication');
+		})
+		.then (() => {
+			dbC = new PouchDB('c');
+			return dbC.get('test');
+		})
+		.then (() => {
+			return dbC.destroy();
+		});
 
-		doc.source = 'b';
-		doc.target = 'c';
-		delete doc._replication_state;
-		const resp2 = await db.put(doc);
-		resp2.ok.should.be.ok;
-
-		await replicationCompletion(db, 'my_replication');
-
-		const dbC = new PouchDB('c');
-		await dbC.get('test');
-		await dbC.destroy();
 	});
 
-	it('delete replication', async () => {
+	function replicationRunning(db, name) {
+		return new Promise (resolve => {
+			const check = (doc) => {
+				if (doc._replication_id) {
+					resolve(doc);
+					return true;
+				}
+
+				return async();
+			};
+			const async = () => db.get(name).then(check);
+
+			async();
+		});
+	}
+
+	it('delete replication', () => {
 		const dbA = new PouchDB('a');
-		await dbA.put({}, 'test1');
+		let dbB;
+		return dbA.put({_id: 'test1'})
+			.then(() => {
+				return db.put(replicationDocument);
+			})
+			.then(resp => {
+				resp.ok.should.be.ok;
+				return replicationRunning(db, 'my_replication');
+			})
+			.then(doc => {
+				return db.remove(doc);
+			})
+			.then(resp2 => {
+				resp2.ok.should.be.ok;
+				return dbA.put({_id: 'test2'});
+			})
+			.then(() => {
+				dbB = new PouchDB('b');
+				return dbB.get('test1');
+			})
+			.then(() => {
+				return dbB.get('test2');
+			})
+			// .then(() => {
+			// 	throw "should not get here";
+			// })
+			.catch(err => {
+				err.status.should.equal(404);
+			});
 
-		const resp = await db.put(replicationDocument);
-		resp.ok.should.be.ok;
+	});
 
-		let doc;
-		do {
-			doc = await db.get('my_replication');
-		} while(!doc._replication_id);
-		const resp2 = await db.remove(doc);
-		resp2.ok.should.be.ok;
+	function replicationTriggered(db, name) {
+		return new Promise (resolve => {
+			const check = (doc) => {
+				if (doc._replication_state === 'triggered') {
+					resolve(doc);
+					return true;
+				}
 
-		dbA.put({}, 'test2');
-		const dbB = new PouchDB('b');
-		await dbB.get('test1');
-		const err = await shouldThrowError(async () => {
-			await dbB.get('test2');
+				return async();
+			};
+			const async = () => db.get(name).then(check);
+
+			async();
 		});
-		err.status.should.equal(404);
+	}
+
+	it('double replication', () => {
+		return db.put(replicationDocument)
+		.then(resp => {
+			resp.ok.should.be.ok;
+			return replicationTriggered(db, 'my_replication');
+		})
+		.then(() => {
+			const repDoc = extend({}, replicationDocument);
+			repDoc._id = 'my_replication2';
+			return db.put(repDoc);
+		})
+		.then(resp => {
+			resp.ok.should.be.ok;
+			return replicationRunning(db, 'my_replication');
+		})
+		.then(doc => {
+			doc.should.have.property('_replication_state');
+			return replicationRunning(db, 'my_replication2');
+		})
+		.then(doc => {
+			doc.should.not.have.property('_replication_state');
+		});
 	});
 
-	it('double resplication', async () => {
-		const resp = await db.put(replicationDocument);
-		resp.ok.should.be.ok;
+	function replicationError(db, name) {
+		return new Promise (resolve => {
+			const check = (doc) => {
+				if (doc._replication_state === 'error') {
+					resolve(doc);
+					return true;
+				}
 
-		// first replication active
-		let doc;
-		do {
-			doc = await db.get('my_replication');
-		} while (!doc._replication_state === 'triggered');
+				return async();
+			};
+			const async = () => db.get(name).then(check);
 
-		// start second
-		const repDoc = extend({}, replicationDocument);
-		repDoc._id = 'my_replication2';
-		const resp2 = await db.put(repDoc);
-		resp2.ok.should.be.ok;
+			async();
+		});
+	}
 
-		let firstDoc, secondDoc;
-		do {
-			firstDoc = await db.get('my_replication');
-			secondDoc = await db.get('my_replication2');
-		} while (!(firstDoc._replication_id && firstDoc._replication_id === secondDoc._replication_id));
-
-		firstDoc.should.have.property('_replication_state');
-		secondDoc.should.not.have.property('_replication_state');
-	});
-
-	it('replication error', async () => {
+	it('replication error', () => {
 		const repDoc = extend({}, replicationDocument);
 		// unlikely couchdb port
 		repDoc.source = 'http://localhost:3423/test';
 		// FIXME: https://github.com/pouchdb/pouchdb-replicator/issues/2
 		repDoc.retry = false;
-		await db.put(repDoc);
 
-		let doc;
-		do {
-			doc = await db.get('my_replication');
-		} while (doc._replication_state !== 'error');
+		return db.put(repDoc)
+		.then(() => {
+			return replicationError(db, 'my_replication');
+		})
+		.then(doc => {
+			doc._replication_state.should.equal('error');
+		});
 	});
 });
