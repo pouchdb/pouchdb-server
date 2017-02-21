55aa1932903083fcaf1b4c53898f98065c8a0600
Nolan Lawson <nolan@nolanlawson.com>
2017-02-20 18:20:38 -0800

    (#207) - try to fix master by downgrading pouchdb

--- bin/test-setup.sh
+++ bin/test-setup.sh
@@ -10,10 +10,11 @@ DIRECTORY='pouchdb-tests'
 if [ ! -d "$DIRECTORY" ]; then
   # Control will enter here if $DIRECTORY exists.
   git clone --single-branch --branch master \
-    --depth 1 \
+    --depth 500 \
     https://github.com/pouchdb/pouchdb.git ${DIRECTORY}
 fi
 
 cd pouchdb-tests
+git checkout 1ccf22988088273013a5954361f4f963f713a612
 npm install
 cd ..
