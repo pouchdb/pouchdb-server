1c8fe26677ab2ebdf306108a45f3460dd59c96a6
Francis Gulotta <wizard@roborooter.com>
2017-06-08 22:27:57 -0400

    Allow passing options to mocha
    This lets you pass `debug` or mocha flags when running unit tests.

--- bin/unit-tests.sh
+++ bin/unit-tests.sh
@@ -3,7 +3,7 @@
 ./packages/node_modules/pouchdb-server/bin/pouchdb-server -m -n -p 5984 $SERVER_ARGS &
 POUCHDB_SERVER_PID=$!
 
-COUCH_HOST=http://localhost:5984 TIMEOUT=120000 mocha ./tests/**/*
+COUCH_HOST=http://localhost:5984 TIMEOUT=120000 mocha ./tests/**/* $1
 
 EXIT_STATUS=$?
 if [[ ! -z $POUCHDB_SERVER_PID ]]; then
