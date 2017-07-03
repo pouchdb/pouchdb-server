9a711ebabf174a24b8708df55783c4adc68c94b6
Gregor <gregor@martynus.net>
2017-07-03 15:16:30 -0700

    pouchdb-size: adapt tests

--- tests/pouchdb-size/test.js
+++ tests/pouchdb-size/test.js
@@ -1,45 +1,53 @@
-"use strict";
+const chai = require('chai');
+const should = chai.should();
+const PouchDB = require('pouchdb');
+const memdown = require('memdown');
+const sqldown = require('sqldown');
+const medeadown = require('medeadown');
+const jsondown = require('jsondown');
+const locket = require('locket');
+const Promise = require('bluebird');
+const fse = Promise.promisifyAll(require("fs-extra"));
 
-/*global describe, it, after, before */
-/*jshint expr: true */
+const PouchDBSize = require('../../packages/node_modules/pouchdb-size');
 
-var chai = require('chai');
-var should = chai.should();
-var PouchDB = require('pouchdb');
-var memdown = require('memdown');
-var sqldown = require('sqldown');
-var medeadown = require('medeadown');
-var jsondown = require('jsondown');
-var locket = require('locket');
-var Promise = require('bluebird');
-var fse = Promise.promisifyAll(require("fs-extra"));
-//pouchdb-size
-PouchDB.plugin(require('../'));
+PouchDB.plugin(PouchDBSize);
 
 describe('pouchdb-size tests', function () {
   before(function () {
     return fse.mkdirAsync("b");
   });
+
   after(function () {
     return new PouchDB("a").destroy().then(function () {
       return new PouchDB('b/chello world!').destroy();
-    }).then(function () {
+    })
+
+    .then(function () {
       return fse.rmdirAsync("b");
-    }).then(function () {
+    })
+
+    .then(function () {
       return new PouchDB("e", {db: sqldown}).destroy();
-    }).then(function () {
+    })
+
+    .then(function () {
       return new PouchDB("./f", {db: medeadown}).destroy();
-    }).then(function () {
+    })
+
+    .then(function () {
       return fse.unlinkAsync("g");
-    }).then(function () {
+    })
+
+    .then(function () {
       return fse.removeAsync("h");
     });
   });
 
   it("should work in the normal case", function (done) {
-    var db1 = new PouchDB('a');
+    const db1 = new PouchDB('a');
     db1.installSizeWrapper();
-    var promise = db1.info(function (err, info) {
+    const promise = db1.info(function (err, info) {
       should.not.exist(err);
 
       info.disk_size.should.be.greaterThan(0);
@@ -49,7 +57,7 @@ describe('pouchdb-size tests', function () {
   });
 
   it("should work with a weird name and a prefix", function (done) {
-    var db2 = new PouchDB('hello world!', {prefix: "b/c"});
+    const db2 = new PouchDB('hello world!', {prefix: "b/c"});
     db2.installSizeWrapper();
     db2.info(function (err, info) {
       should.not.exist(err);
@@ -60,14 +68,14 @@ describe('pouchdb-size tests', function () {
   });
 
   it("shouldn't disrupt a non-leveldb leveldown adapter", function (done) {
-    var db3 = new PouchDB('d', {db: memdown});
+    const db3 = new PouchDB('d', {db: memdown});
     db3.installSizeWrapper();
     db3.info(function (err, info) {
       should.not.exist(err);
       should.not.exist(info.disk_size);
       info.db_name.should.equal("d");
 
-      var promise = db3.getDiskSize(function (err, size) {
+      const promise = db3.getDiskSize(function (err, size) {
         //getDiskSize() should provide a more solid error.
         err.should.exist;
         should.not.exist(size);
@@ -80,7 +88,7 @@ describe('pouchdb-size tests', function () {
 
   it("shouldn't disrupt non-leveldown adapter", function (done) {
     //mock object
-    var db4 = {
+    const db4 = {
       type: function () {
         return "http";
       }
@@ -94,7 +102,7 @@ describe('pouchdb-size tests', function () {
   });
 
   it("should work with sqldown", function (done) {
-    var db = new PouchDB("e", {db: sqldown});
+    const db = new PouchDB("e", {db: sqldown});
     db.installSizeWrapper();
 
     db.getDiskSize().then(function (resp) {
@@ -111,7 +119,7 @@ describe('pouchdb-size tests', function () {
 
   it("should work with medeadown", function (done) {
     // ./f instead of f is needed for medeadown.
-    var db = new PouchDB("./f", {db: medeadown});
+    const db = new PouchDB("./f", {db: medeadown});
     db.installSizeWrapper();
 
     db.info().then(function (info) {
@@ -123,7 +131,7 @@ describe('pouchdb-size tests', function () {
   });
 
   it("should work with jsondown", function (done) {
-    var db = new PouchDB("g", {db: jsondown});
+    const db = new PouchDB("g", {db: jsondown});
     db.installSizeWrapper();
 
     db.info().then(function (info) {
@@ -135,7 +143,7 @@ describe('pouchdb-size tests', function () {
   });
 
   it("should work with locket", function (done) {
-    var db = new PouchDB("h", {db: locket});
+    const db = new PouchDB("h", {db: locket});
     db.installSizeWrapper();
 
     db.info().then(function (info) {
