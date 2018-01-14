6ba1c93d77ee4dac2bc075b044f956666025dcaf
Loïc Bistuer <loic.bistuer@gmail.com>
2018-01-15 01:38:03 +0700

    Tweak the express-pouchdb mode to work with the latest PouchDB test suite.

--- bin/express-pouchdb-minimum-for-pouchdb.js
+++ bin/express-pouchdb-minimum-for-pouchdb.js
@@ -3,6 +3,11 @@
 var PouchDB = require('pouchdb');
 
 var app = require('../packages/node_modules/express-pouchdb')(PouchDB, {
-  mode: 'minimumForPouchDB'
+  mode: 'minimumForPouchDB',
+  overrideMode: {
+    'include': [
+      'validation'
+    ]
+  }
 });
 app.listen(6984);
