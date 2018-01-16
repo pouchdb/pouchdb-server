92737a10aa0dfcbf04d17d68bfae77a1e7309270
Loïc Bistuer <loic.bistuer@gmail.com>
2018-01-16 23:40:14 +0700

    Further tweak the express-pouchdb mode used in the test suite.
    This partially reverts 6ba1c93d77ee4dac2bc075b044f956666025dcaf as it appears better to only modify the test runner rather than the actual API.

--- bin/express-pouchdb-minimum-for-pouchdb.js
+++ bin/express-pouchdb-minimum-for-pouchdb.js
@@ -4,9 +4,12 @@ var PouchDB = require('pouchdb');
 
 var app = require('../packages/node_modules/express-pouchdb')(PouchDB, {
   mode: 'minimumForPouchDB',
+  // Interim overrides until the pouchdb test suite properly detects
+  // the distinction between the fullCouchDB/minimumForPouchDB modes.
   overrideMode: {
     'include': [
-      'validation'
+      'validation',
+      'routes/replicate'
     ]
   }
 });
